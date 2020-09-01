c9eb52407959c760deeeab15ebd35855b6c721e2
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-01 07:41:37 -0700

    ADDED - Find player function to DB

--- data/database.js
+++ data/database.js
@@ -1,5 +1,15 @@
 let database = [];
 
+function findPlayer (payload) {
+    if (database.length > 0) {
+        let player = database.find(el => el.player.user.username === payload || el.player.nickname === payload);
+        return player;
+    } else {
+        return undefined;
+    }
+
+}
+
 function addToDB (payload) {
     database.push(payload);
 }
@@ -8,4 +18,4 @@ function clearDB () {
     database.length = 0;
 }
 
-module.exports = {database, addToDB, clearDB};
+module.exports = {database, addToDB, clearDB, findPlayer};
