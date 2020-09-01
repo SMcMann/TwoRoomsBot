e6bf7a0ea02d23e7d09f2c4d64aba145ba57157c
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-01 03:35:45 -0700

    ADDED - All requested cards

---
+++ image/cards.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+
+const president = new Discord.MessageAttachment('./image/president.png');
+const bomber = new Discord.MessageAttachment('./image/bomber.png');
+const doctor = new Discord.MessageAttachment('./image/doctor.png');
+const engineer = new Discord.MessageAttachment('./image/enginner.png');
+const redspy = new Discord.MessageAttachment('./image/spy_r.png');
+const bluespy = new Discord.MessageAttachment('./image/spy_b.png');
+const bluecoyboy = new Discord.MessageAttachment('./image/blue.png');
+const redcoyboy = new Discord.MessageAttachment('./image/red.png');
+const blueshyguy = new Discord.MessageAttachment('./image/shy_guy_b.png');
+const redshyguy = new Discord.MessageAttachment('./image/shy_guy_r.png');
+const redpsychologist = new Discord.MessageAttachment('./image/psycologist_r.png');
+const bluepsychologist = new Discord.MessageAttachment('./image/psycologist_b.png');
+const gambler = new Discord.MessageAttachment('./image/gambler.png');
+const decoy = new Discord.MessageAttachment('./image/decoy.png');
+const sniper = new Discord.MessageAttachment('./image/sniper.png');
+const blueteam = new Discord.MessageAttachment('./image/blue.png');
+const redteam = new Discord.MessageAttachment('./image/red.png');
+
+
+module.exports = { 
+    president,
+    bomber, 
+    doctor, 
+    engineer, 
+    redspy,
+    bluespy,
+    bluecoyboy,
+    redcoyboy,
+    blueshyguy,
+    redshyguy,
+    redpsychologist,
+    bluepsychologist,
+    gambler,
+    decoy,
+    sniper,
+    blueteam,
+    redteam
+}
