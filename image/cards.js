fe947a84b65f1c74154b8cb78e4811c13f4cf6d5
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-06 16:54:49 -0700

    MINOR - Switched to URL

--- image/cards.js
+++ image/cards.js
@@ -1,22 +1,22 @@
 const Discord = require('discord.js');
 
-const president = new Discord.MessageAttachment('./image/president.png');
-const bomber = new Discord.MessageAttachment('./image/bomber.png');
-const doctor = new Discord.MessageAttachment('./image/doctor.png');
-const engineer = new Discord.MessageAttachment('./image/engineer.png');
-const redspy = new Discord.MessageAttachment('./image/spy_r.png');
-const bluespy = new Discord.MessageAttachment('./image/spy_b.png');
-const bluecoyboy = new Discord.MessageAttachment('./image/blue.png');
-const redcoyboy = new Discord.MessageAttachment('./image/red.png');
-const blueshyguy = new Discord.MessageAttachment('./image/shy_guy_b.png');
-const redshyguy = new Discord.MessageAttachment('./image/shy_guy_r.png');
-const redpsychologist = new Discord.MessageAttachment('./image/psycologist_r.png');
-const bluepsychologist = new Discord.MessageAttachment('./image/psycologist_b.png');
-const gambler = new Discord.MessageAttachment('./image/gambler.png');
-const decoy = new Discord.MessageAttachment('./image/decoy.png');
-const sniper = new Discord.MessageAttachment('./image/sniper.png');
-const blueteam = new Discord.MessageAttachment('./image/blue.png');
-const redteam = new Discord.MessageAttachment('./image/red.png');
+const president = 'https://cdn.discordapp.com/attachments/749887204914102322/752305040525623326/president.png';
+const bomber = 'https://cdn.discordapp.com/attachments/749887204914102322/752305020032254073/bomber.png';
+const doctor = 'https://cdn.discordapp.com/attachments/749887204914102322/752312155277230290/doctor.png';
+const engineer = 'https://cdn.discordapp.com/attachments/749887204914102322/752312178966528080/engineer.png';
+const redspy = 'https://cdn.discordapp.com/attachments/749887204914102322/752312530357059605/spy_r.png';
+const bluespy = 'https://cdn.discordapp.com/attachments/749887204914102322/752312456084193301/spy_b.png';
+const bluecoyboy = 'https://cdn.discordapp.com/attachments/749887204914102322/752304749986054274/blue.png';
+const redcoyboy = 'https://cdn.discordapp.com/attachments/749887204914102322/752304911638593596/red.png';
+const blueshyguy = 'https://cdn.discordapp.com/attachments/749887204914102322/752311941413863445/shy_guy_b.png';
+const redshyguy = 'https://cdn.discordapp.com/attachments/749887204914102322/752311943313752075/shy_guy_r.png';
+const redpsychologist = 'https://cdn.discordapp.com/attachments/749887204914102322/752311189958164480/psycologist_r.png';
+const bluepsychologist = 'https://cdn.discordapp.com/attachments/749887204914102322/752311014233604146/psycologist_b.png';
+const gambler = 'https://cdn.discordapp.com/attachments/749887204914102322/752311421089611876/gambler.png'
+const decoy = 'https://cdn.discordapp.com/attachments/749887204914102322/752311714963259451/target.png';
+const sniper = 'https://cdn.discordapp.com/attachments/749887204914102322/752311686278676580/sniper.png'
+const blueteam = 'https://cdn.discordapp.com/attachments/749887204914102322/752304749986054274/blue.png';
+const redteam = 'https://cdn.discordapp.com/attachments/749887204914102322/752304911638593596/red.png';
 
 
 module.exports = { 
