0d912e22f56795371c10443ed842b91cc4652429
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-09-08 16:58:54 -0700

    ADDED - Lobby

--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,16 @@
 const { channels } = require("../data/serverValues")
 let room1
 let room2
+let lobby
 
 function setTextRooms(client) {
     room1 = client.channels.cache.find(channel => channel.name === channels.text1);
     room2 = client.channels.cache.find(channel => channel.name === channels.text2);
+    lobby = client.channels.cache.find(channel => channel.name === channels.textLobby);
 }
 
 function getRooms() {
-    return { room1, room2 }
+    return { room1, room2, lobby }
 }
 
 
