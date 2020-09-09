f6c432f116bd8b22b14427901eef9ab3e16032d8
jacobs463 <jsonntag1@sycamores.indstate.edu>
2020-09-09 17:06:29 -0400

    ADDED - Room card/color share

--- commands/reveal.js
+++ commands/reveal.js
@@ -1,10 +1,14 @@
 const { findPlayer, checkLive } = require('../data/database');
 const { getUserFromArgs } = require('../scripts/args');
 const { shareColor, shareCard } = require('../scripts/shareFuctions');
+const { getRooms } = require("../scripts/client");
+const { channels } = require("../data/serverValues");
+const { getCard } = require("../image/cards");
 
 const cmdError = `**Command Stucture:** \`!share card <user>\`\nYou must specify what type of share you want to do.\n - color\n - card`
 const cardAliases = ['role', 'all'];
 const colorAliases = ['colour', 'team'];
+const roomAliases = ['room'];
 
 module.exports = {
     name: 'reveal', //THIS MUST BE THE SAME NAME OF THE FILE/COMMAND
@@ -27,7 +31,36 @@ module.exports = {
         if (colorAliases.some(el => el === cmd.toLowerCase())) cmd = 'color';
         if (cardAliases.some(el => el === cmd.toLowerCase())) cmd = 'card';
 
+        
+        let initiator = findPlayer(message.author.username); // Finds the DB save of current user
+
         args.shift(); // Removes the command Arg
+        if (roomAliases.some(el => el === args[0].toLowerCase())) {
+            const clientRooms = getRooms();
+            let thisRoom;
+            if (initiator.currChannel == channels.room1) thisRoom = clientRooms.room1; 
+            else thisRoom = clientRooms.room2;
+            //Share with the room
+            if (cmd == "color") {
+                thisRoom.send(`${initiator.player.user.username} is sharing their color with the room! They are ${initiator.character.color}.`)
+                    .then(sentMessage => {
+                        sentMessage.delete({timeout: 10000})
+                    })
+                    .catch(console.error);
+            } else {
+                thisRoom.send(`${initiator.player.user.username} is sharing their card with the room!`)
+                    .then(sentMessage => {
+                        sentMessage.delete({timeout: 10000});
+                        thisRoom.send({embed: getCard(initiator.character,true)})
+                            .then(embedMessage => {
+                                embedMessage.delete({timeout: 10000});
+                            })
+                            .catch(console.error);
+                    })
+                    .catch(console.error);
+            }
+            return;
+        }
         let user = getUserFromArgs(args) // Creates target user variable
 
         // If statement checks for a mention instead of a user, and switches the mention for the mentioned user
@@ -38,7 +71,6 @@ module.exports = {
         }
 
         let target = findPlayer(user); // Finds the DB save of the target
-        let initiator = findPlayer(message.author.username); // Finds the DB save of current user
         
         // Invalid user check
         if (target !== undefined) {
